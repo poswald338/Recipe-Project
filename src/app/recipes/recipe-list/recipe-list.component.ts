caff13920896a16981a9782194c978dfc3bd2032
poswald338 <poswald338@gmail.com>
2021-04-29 20:09:12 -0600

    Aligning images
    Missing dash in div class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test.', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png'),
+    new Recipe('A Test Recipe', 'This is simply a test.', 'https://www.disneyfoodblog.com/wp-content/uploads/2013/10/MacAndCheeseTop.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test.', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_1280.png')
   ];
 
