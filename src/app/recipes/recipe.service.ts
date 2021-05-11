253dc8260a16703bbe1a79d828ce5800ba68d861
poswald338 <poswald338@gmail.com>
2021-05-10 19:04:38 -0600

    Services Added
    added a recipeService and a shoppingListService.

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingService } from "../shopping-list/shopping.service";
+
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Quesadilla',
+      'A delicious chicken, steak, and cheese quesadilla!',
+      'https://cdn.pixabay.com/photo/2017/03/07/18/21/quesadillas-2124910__340.jpg',
+      [
+        new Ingredient('tortilla', 1),
+        new Ingredient('meat', 2),
+        new Ingredient('cheese', 1)
+      ]),
+    new Recipe(
+      'Pepperoni Pizza Rolls',
+      'Tasty bite sized pizza rolls!',
+      'https://cdn.pixabay.com/photo/2015/04/08/13/14/pizza-712667_1280.jpg',
+      [
+        new Ingredient('crust', 1),
+        new Ingredient('pepperoni', 5),
+        new Ingredient('cheese', 1)
+      ])
+  ];
+
+  constructor(private shoppingService: ShoppingService){}
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+
+  recipesToShoppingList(ingredients: Ingredient[]){
+    this.shoppingService.addIngredients(ingredients);
+  }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingService } from './shopping.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,18 +8,20 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients: Ingredient[];
 
-  constructor() { }
+
+  constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit(): void {
+    this.ingredients = this.shoppingService.getIngredients();
+    this.shoppingService.ingredientsChanged
+      .subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      )
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
-  }
 
 }
