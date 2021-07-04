c79250485588e76d3c7aac39704b9efff97e6f26
poswald338 <poswald338@gmail.com>
2021-07-04 11:54:24 -0600

    made changes allowing angular universal to work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule} from '@ngrx/store-devtools'
+import { StoreDevtoolsModule} from '@ngrx/store-devtools';
 import { AuthEffects } from './auth/store/auth.effects';
 import { environment } from '../environments/environment';
 import {StoreRouterConnectingModule} from '@ngrx/router-store';
-import { RecipeEffects } from './recipes/store/recipe.effects'
-
+import { RecipeEffects } from './recipes/store/recipe.effects';
+import { CoreModule } from './core.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,11 +25,11 @@ import { RecipeEffects } from './recipes/store/recipe.effects'
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    CoreModule,
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects, RecipeEffects]),
     StoreDevtoolsModule.instrument({logOnly: environment.production}),
-    StoreRouterConnectingModule.forRoot()
+    StoreRouterConnectingModule.forRoot(),
+    CoreModule
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
